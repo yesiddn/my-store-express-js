0fce3af5b0d09dc6665874841717e10c12aa6353
Yesid <yesidrodriguez305@gmail.com>
2023-09-09 20:02:30 -0500

    Add installation of Express and creation of an HTTP server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('My App');
+// Example: Hello, my server in express! (https://expressjs.com/en/starter/hello-world.html)
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, my server in express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`); // se marca el console.log() por eslint, ya que es una mala practica en produccion
+});
