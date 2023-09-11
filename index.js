57b453d107d150de0d3bcd76b07b2fb700e0515a
Yesid <yesidrodriguez305@gmail.com>
2023-09-10 20:56:50 -0500

    Add routing with Express

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ app.get('/', (req, res) => {
   res.send('Hello, my server in express!');
 });
 
+app.get('/new-endpoint', (req, res) => {
+  res.send('Hello, this is a new endpoint!');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'Product 1',
+    price: 1000
+  });
+});
+
+app.get('/categories', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'Category 1'
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`); // se marca el console.log() por eslint, ya que es una mala practica en produccion
 });
