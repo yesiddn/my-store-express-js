07f87be0e8a4de7f635f16bb2b90d64537574ae9
Yesid <yesidrodriguez305@gmail.com>
2023-09-11 11:42:38 -0500

    Add parameters to the routing

--- index.js
+++ index.js
@@ -12,19 +12,87 @@ app.get('/new-endpoint', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  /* const params = req.params;
+  {
+    params: {
+      id: 1
+    }
+  }
+  */
+
+  // const id = req.params.id;
+  const { id } = req.params; // forma destructurada
   res.json({
+    id,
+    name: 'Product 2',
+    price: 2000,
+  });
+}); // :id es un parametro
+
+app.get('/categories', (req, res) => {
+  res.json([
+    {
     id: 1,
-    name: 'Product 1',
-    price: 1000
+    name: 'Category 1',
+    },
+    {
+      id: 2,
+      name: 'Category 2',
+    }
+  ]);
+});
+
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Category 1',
   });
 });
 
-app.get('/categories', (req, res) => {
+app.get('/people', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Person 1',
+    },
+    {
+      id: 2,
+      name: 'Person 2',
+    },
+  ]);
+});
+
+app.get('/people/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    id: 1,
-    name: 'Category 1'
+    id,
+    name: 'Person 1',
   });
 });
+
+// app.get('/categories/:categoryId/products/:productId', (req, res) => {
+//   const { categoryId, productId } = req.params;
+//   res.json({
+//     categoryId,
+//     productId,
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`); // se marca el console.log() por eslint, ya que es una mala practica en produccion
