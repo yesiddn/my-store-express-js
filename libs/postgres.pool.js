cdc047659f8f7cb1252ced35afd94d242c4abb3a
Yesid <yesidrodriguez305@gmail.com>
2023-09-26 13:48:21 -0500

    Add environment variables

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,23 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'jess',
-  password: 'admin',
-  database: 'my_store',
-});
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+
+// url de conexion a la base de datos
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+// configuracion de la conexion automatica
+const pool = new Pool({ connectionString: URI });
+
+// configuracion de la conexion manual
+// const pool = new Pool({
+//   host: 'localhost',
+//   port: 5432,
+//   user: 'jess',
+//   password: 'admin',
+//   database: 'my_store',
+// });
 
 module.exports = pool;
